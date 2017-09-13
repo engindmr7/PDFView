e47ca0ff388d746ffe852ac007b38aeea664b672
engindmr7 <engindmr7@gmail.com>
2017-09-13 12:51:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/engindemir/pdfview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.engindemir.pdfview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        pdf.fromAsset("Dekont.pdf").load()
+    }
+}
